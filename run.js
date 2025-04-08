843c808fa657821a86d937183d25b61ca449839c
echortech <147915250+echortech@users.noreply.github.com>
2025-04-08 11:20:51 +0530

    4,5 changes

--- run.js
+++ run.js
@@ -9,23 +9,51 @@
 //          }
 // }
 //   console.log(row);
-let n = 7;
-for (let i = n; i >= 1; i--) {
-  let row = '*';
-  for (let j = 1; j < i; j++) {
-    row += '-*';
-  }
-  console.log(row);
-}
-for (let i = 2; i <= n; i++) {
-  let row = '*';
-  for (let j = 1; j < i; j++) {
-    row += '-*';
-  }
-  console.log(row);
-}
 
-//code here
+//problem 3
+// let n = 7;
+// for (let i = n; i >= 1; i--) {
+//   let row = '*';
+//   for (let j = 1; j < i; j++) {
+//     row += '-*';
+//   }
+//   console.log(row);
+// }
+// for (let i = 2; i <= n; i++) {
+//   let row = '*';
+//   for (let j = 1; j < i; j++) {
+//     row += '-*';
+//   }
+//   console.log(row);
+// }
+
+//problem 4
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let row = '  '.repeat(n - i); 
+//   for (let j = 1; j <= i; j++) {
+//     row += '* ';
+//   }
+//   console.log(row);
+// }
+
+// problem 5
+// let n = 5
+// for (let i = n; i>=1; i--){
+//   let row = '  '.repeat(n-i);
+//   for (let j= 1; j<=1;j++){
+//     row += '* '.repeat(i);
+//   }
+//   console.log(row);
+// }
+
+//problem 6
+
+
+
+
+//code here 
 // use only for loops. dont use .repeat() or .trim()
 
 // expected output:
---
+++ script.js
@@ -0,0 +1,41 @@
+//right traingle
+// let n = 10; // you can change this to 10 or any number
+// for (let i = 1; i <= n; i++) {
+//   let row = '';
+//   for (let j = 1; j <= i; j++) {
+//     row += '*';
+//   }
+//   console.log(row);
+// }
+
+//inverted traingle
+// let n = 10;                                
+// for (let i = n; i >= 1; i--) {
+//   let row = '';
+//   for (let j = 1; j <= i; j++) {
+//     row += '*';
+//   }
+//   console.log(row);
+// }
+
+//pyramid
+// let n = 6;
+// for (let i = 1; i <= n; i++) {
+//   let row = '';
+//   row += ' '.repeat(n - i);       // add spaces
+//   row += '*'.repeat(2 * i - 1);   // add stars
+//   console.log(row);
+//}
+
+
+//inverted pyramid
+// let n = 6;
+
+// for (let i = 0; i < n; i++) {
+//   let row = '';
+//   row += ' '.repeat(i);                  // adds spaces at the beginning
+//   row += '*'.repeat(2 * (n - i) - 1);    // adds stars
+//   console.log(row);
+// }
+
+
