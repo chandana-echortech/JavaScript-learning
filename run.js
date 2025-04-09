eaf5f710962ae1d8ff7a0978d4f6677f4223cb64
echortech <147915250+echortech@users.noreply.github.com>
2025-04-09 15:59:32 +0530

    NotepadChanges

--- run.js
+++ run.js
@@ -154,43 +154,58 @@
 // }
 
 // // problem 11
-let n = 5;
+// let n = 5;
 
-for (let i = 1; i <= n; i++) {
-  let row = '';
+// for (let i = 1; i <= n; i++) {
+//   let row = '';
   
-  for (let j = 1; j<= (n - i) * 2; j++) {
-    row += '-';
-  }
- for (let k = 1; k<= i; k++) {
-    if (k == 1) {
-      row += '*';
-    } else {
-      row += '-*';
-    }
-  }
-console.log(row);
-}
-for (let i = n - 1; i >= 1; i--) {
-  let row = '';
+//   for (let j = 1; j<= (n - i) * 2; j++) {
+//     row += '-';
+//   }
+//  for (let k = 1; k<= i; k++) {
+//     if (k == 1) {
+//       row += '*';
+//     } else {
+//       row += '-*';
+//     }
+//   }
+// console.log(row);
+// }
+// for (let i = n - 1; i >= 1; i--) {
+//   let row = '';
 
-  for (let j = 1; j <= (n - i) * 2; j++) {
-    row += '-';
-  }
-  for (let k= 1; k<= i; k++) {
-    if (k == 1) {
-      row += '*';
-    } else {
-      row += '-*';
-    }
-  }
+//   for (let j = 1; j <= (n - i) * 2; j++) {
+//     row += '-';
+//   }
+//   for (let k= 1; k<= i; k++) {
+//     if (k == 1) {
+//       row += '*';
+//     } else {
+//       row += '-*';
+//     }
+//   }
 
-  console.log(row);
-}
+// //   console.log(row);
+// // }
+// let n = 5; 
+
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+
+  
+//   for (let j = 1; j <= n - i; j++) {
+//     row += " ";
+//   }
+
+//   for (let k = 1; k <= 2 * i - 1; k++) {
+//     row += "*";
+//   }
+
+//   console.log(row);
+// }
 
 
-
-
+ 
 //pyramid
 
 // let n = 5;
@@ -230,3 +245,134 @@ for (let i = n - 1; i >= 1; i--) {
 // * *
 // *
 
+
+
+
+//09-04-2025
+//inverted pyramid
+
+// let n=5;
+// for (let i=n; i>=1; i--){
+//     let row = '';
+//     for (let j=1 ; j<=n-i;j++){
+//         row += ' ';
+//     }
+// for (let k =1; k<=i; k++){
+//     row += '* '
+// }
+// console.log (row);
+// }
+
+//Left-Aligned Decreasing Triangle
+
+// let n = 5;
+// for (let i=n; i>=1; i--){
+//     let row = '';
+//     for(let j = 1; j<=n ; j++){
+//         row+= '';
+//    }
+//    for (let k = 1; k<=i ; k++){
+//     if ( k === i) {row +='*'
+//     }else{
+//       row+='*-';
+//     }
+  
+
+//    }
+//    console.log(row);
+// }
+
+//Left-Aligned Increasing Triangle
+// let n = 5;
+// for (let i =1; i<=n; i++){
+//     let row = '';
+//     for (let j = 1; j<=i; j++){
+//         if ( j === i) {row +='*'
+//         }else{
+//           row+='*-';
+//         }
+      
+  
+//     }
+//     console.log(row);
+// }
+
+//pyramid
+// let n = 5;
+// for (let i= 1; i<=n ; i++){
+//     let row ='';
+//     for (let j = 1; j<=n-i;j++){
+//         row+=' ';
+//     }
+//     for (let k=1 ; k<=i; k++){
+//         if ( k === i) {row +='*'
+//                       }else{
+//                         row+='*-';
+//                       }
+                    
+//                 }
+    
+//     console.log(row);
+// }
+
+//Right-Aligned Increasing Triangle
+// let n =5;
+// for (let i=1; i<=n;i++){
+//     let row = '';
+//     for (let j=1; j<=n-i;j++){
+//         row +='--';
+//     }
+//     for (let k=1; k<=i ; k++){
+//         if(k === i) {
+//             row += '*'
+//         }else{
+//         row +='*-'}
+//     }
+//     console.log(row);
+// }
+
+//Right-Aligned Decreasing Triangle
+
+// let n = 5;
+// for (let i=n; i>=1; i--){
+//     let row = '';
+//     for (let j=1; j<=n-i ;j++){
+//         row+='  ';
+//     }
+//     for (let k=1; k<=i; k++){
+//         if ( k === i) {row +='*'
+//           }else{
+//             row+='*-';
+//           }
+        
+//     }
+//     console.log(row);
+// }
+
+// let n = 5;
+// for ( let i = n ; i>=1; i--){
+//     let row = '';
+// for (let j = 1; j<=n-i; j++) {
+//     row+= ' ';
+// }
+// for (let k = 1; k<=i; k++){
+//     if ( k === i) {row +='*'
+//                   }else{
+//                     row+='*-';
+//                   }
+//                 }
+// console.log(row);
+// }
+// for ( let i = 2 ; i<=n; i++){
+//     let row = '';
+// for (let j = 1; j<=n-i; j++) {
+//     row+= ' ';
+// }
+// for (let k = 1; k<=i; k++){
+//     if ( k === i ) {
+//         row+='*'
+//     }else{
+// row+= '*-';
+// }}
+// console.log(row);
+// }
